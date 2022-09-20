30153672ad5463eff32d342d42aef1c27b2aef84
Prabhaharan-M <114008695+Prabhaharan-M@users.noreply.github.com>
2022-09-20 21:29:34 +0530

    Add files via upload

---
+++ src/sample/demo.java
@@ -0,0 +1,22 @@
+package sample;
+
+public class demo {
+
+	public static void main(String[] args) {
+		int a =2;
+		int b = Integer.parseInt(args[4]);
+		System.out.println("haiii value in a is: "+a);
+		System.out.println("The value in b is: "+b);
+		int c = Integer.parseInt(args[0]);
+		int d = Integer.parseInt(args[1]);
+		 int sum = c+d;
+		System.out.println("addition of c and d is" +sum);
+//		int a = Integer.parseInt(args[0]);
+//		int b = Integer.parseInt(args[1]); 
+//		System.out.println( args[0] +  "   " +args[1]);
+//		System.out.println(a+b);
+//
+//		System.out.println( "thanku ..");
+	}
+
+}
